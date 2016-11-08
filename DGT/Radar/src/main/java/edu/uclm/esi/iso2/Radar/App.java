e3af761055ac3b3c9c6bd50e724e0a06d705a592
JoseMiguelTorralbo <JoseMiguel.Torralbo@alu.uclm.es>
2016-11-08 13:25:08 +0100

    Proyecto Maven DGT Iteración 0 (importacion código y creación de los módulos del proyecto)

---
+++ DGT/Radar/src/main/java/edu/uclm/esi/iso2/Radar/App.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.iso2.Radar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DGT/multas/src/main/java/edu/uclm/esi/iso2/multas/dao/DriverDao.java
@@ -0,0 +1,33 @@
+package edu.uclm.esi.iso2.multas.dao;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+
+import edu.uclm.esi.iso2.multas.domain.Driver;
+
+public class DriverDao extends GeneralDao<Driver> {
+	public DriverDao() {
+		super();
+	}
+	
+	public Driver findByDni(String dni) throws HibernateException {
+		Driver driver = null;
+        try {
+            startOperation();
+            
+            Query query=(Query) session.createQuery("from Driver where dni=?");
+            query.setParameter(0, dni);
+            
+            driver = (Driver) query.getSingleResult();
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw e;
+        } finally {
+            HibernateFactory.close(session);
+        }
+        return driver;
+	}
+}
